d48fa1125eae183b580b093217c2e0d7fdd0b00b
POTDAR <amar.potdar@capgemini.com>
2019-08-30 18:46:47 +0530

    first commit, added header strings to src file

---
+++ the_garden_nerd.py
@@ -0,0 +1,15 @@
+'''
+Filename: f:\Dataset\forFatNinja\hEarth_dQuest_flowerRecg\HE_Challenge_data\the_garden_nerd.py
+Path: f:\Dataset\forFatNinja\hEarth_dQuest_flowerRecg\HE_Challenge_data
+Created Date: Friday, August 30th 2019, 6:34:17 pm
+Author: apotdar
+'''
+
+"""[Flower Recognition - Deep Learning]
+The application of deep learning is rapidly growing in the field of computer vision and is helping in building powerful classification and identification models. 
+We can leverage this power of deep learning to build models that can classify and differentiate between different species of flower as well.
+
+Total 102 categories of flowers:
+    train - Contains 18540 images
+    test  - Contains 2009 images
+"""
